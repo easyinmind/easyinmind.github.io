3fa19b07e0f65649c6c8eed9e981a4f03c4ef1ce
easyinmind <yanjunqimind@126.com>
2021-03-04 16:23:34 +0800

    lay0ut

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -8,39 +8,47 @@ module.exports = [
       { text: "🦍 JavaScript", link: "/javascript/" },
       { text: "🐎 工程化相关", link: "/engineering/" },
       { text: "🐘 框架&源码", link: "/code/" },
-      { text: "🦥 多端", link: "/moreend/" },
+      { text: "🦥 运行环境", link: "/moreend/" },
+      {
+        text: "🦙 项目&其它",
+        link: "/feother/",
+      },
     ],
   },
   {
-    text: "🌳 计算机",
+    text: "🌳 编程",
     items: [
-      { text: "🦜 网络协议", link: "/network/" },
-      { text: "🕊 设计模式", link: "/patterns/" },
-      { text: "🦃 数据结构&算法", link: "/data/" },
+      {
+        text: "计算机基础",
+        items: [
+          { text: "🦜 网络协议", link: "/network/" },
+          { text: "🕊 设计模式", link: "/patterns/" },
+          { text: "🦃 数据结构&算法", link: "/data/" },
+        ],
+      },
+      {
+        text: "后端&运维",
+        items: [
+          { text: "🦀 nodejs", link: "/nodejs/" },
+          { text: "🐢 运维相关", link: "/ops/" },
+          { text: "🐋 数据库", link: "/db/" },
+        ],
+      },
     ],
   },
   {
-    text: "🌲后端&运维",
-    items: [
-      { text: "🦀 后端", link: "/nodejs/" },
-      { text: "🐢 运维相关", link: "/ops/" },
-      { text: "🐋 数据库", link: "/db/" },
-    ],
-  },
-  {
-    text: "🌵 其它",
-    items: [
-      { text: "🦉 笔记", link: "/note/" },
-    ],
+    text: "🌵 阅读",
+    items: [{ text: "🦉《你不知道的js》", link: "/book1/" }],
   },
   {
     text: "🎋 导航",
     link: "/archives/",
     items: [
+      { text: "🍉 日常记录", link: "/live/" },
       { text: "🍋 分类", link: "/categories/" },
       { text: "🍇 标签", link: "/tags/" },
       { text: "🥑 归档", link: "/archives/" },
-      { text: "🍏 关于", link: "/about/" }
+      { text: "🍏 关于", link: "/about/" },
     ],
   },
 ];
