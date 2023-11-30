6a5a51ac57653bab469b39c61e919adb4c15ca21
yanjunqi <yanjunqi@360.cn>
2023-11-30 15:41:27 +0800

    dby于 2023-11-30 15:41:27 更新

--- utils/modules/readFileList.js
+++ utils/modules/readFileList.js
@@ -1,9 +1,11 @@
+
 /**
  *  读取所有md文件数据
  */
 const fs = require('fs'); // 文件模块
 const path = require('path'); // 路径模块
 const docsRoot = path.join(__dirname, '..', '..', 'docs'); // docs文件路径
+const chalk = require("chalk"); // 命令行打印美化
 
 function readFileList(dir = docsRoot, filesList = []) {
   const files = fs.readdirSync(dir);
@@ -24,7 +26,7 @@ function readFileList(dir = docsRoot, filesList = []) {
             name = fileNameArr[1]
             type = fileNameArr[2]
           } else { // 超过两个‘.’的
-            log(chalk.yellow(`warning: 该文件 "${filePath}" 没有按照约定命名，将忽略生成相应数据。`))
+            console.log(chalk.yellow(`warning: 该文件 "${filePath}" 没有按照约定命名，将忽略生成相应数据。`))
             return
           }
           if(type === 'md'){ // 过滤非md文件
@@ -37,6 +39,7 @@ function readFileList(dir = docsRoot, filesList = []) {
         }
       }        
   });
+  console.log("filesList123=", filesList)
   return filesList;
 }
 
